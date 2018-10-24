5a213e1cd1229320601e69219909ebdfbaa1d351
CaChan <caitlinchan0@gmail.com>
2018-10-24 10:20:17 -0700

    Complete Program

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,14 @@
-class NamedCow
+class NamedCow extends Cow 
 {
-
+  private String myName;
+  public NamedCow(String type, String name, String sound)
+  {
+    myName = name;
+    myType = "cow";
+    mySound = "moo";
+  }
+  public String getName()
+  {
+    return myName;
+   }
 }
